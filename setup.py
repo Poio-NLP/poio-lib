944f0531b201121b488abb5fdc4dca3d8a76a9c4
Peter Bouda <hey@peterbouda.eu>
2019-11-08 09:23:10 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/Poio-NLP/poio-lib"
 EMAIL = "pbouda@outlook.com"
 AUTHOR = "Peter Bouda"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 # What packages are required for this module to be executed?
 REQUIRED = ["requests", "beautifulsoup4"]
